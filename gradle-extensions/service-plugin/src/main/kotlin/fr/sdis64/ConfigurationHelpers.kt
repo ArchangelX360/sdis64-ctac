224e40c9a51daf6f368a93a480a421729285f04c
ArchangelX360 <titouan.bion@gmail.com>
2023-11-04 13:39:54 +0100

    Small refactoring + visibility fix

---
+++ gradle-extensions/service-plugin/src/main/kotlin/fr/sdis64/ConfigurationHelpers.kt
@@ -0,0 +1,35 @@
+package fr.sdis64
+
+import org.gradle.api.Project
+import org.gradle.api.provider.Provider
+
+// FIXME: this should use `ProviderFactory` instead, but because of https://github.com/gradle/gradle/issues/23572
+//  we need to find a way to make it work with `local.properties`
+// private fun ProviderFactory.propOrEnvProvider(propName: String, envVar: String? = null): Provider<Result<String>> {
+//    val err =
+//        Result.failure<String>(IllegalStateException("either property '$propName' or environment variable $envVar must be defined"))
+//    return gradleProperty(propName)
+//        .orElse(environmentVariable(envVar ?: ""))
+//        .map { s ->
+//            when {
+//                s.isNotBlank() -> Result.success(s)
+//                else -> err
+//            }
+//        }.orElse(err)
+// }
+private fun Project.propOrEnvProvider(propName: String, envVar: String? = null): Provider<Result<String>> {
+    val err =
+        Result.failure<String>(IllegalStateException("either property '$propName' or environment variable $envVar must be defined"))
+    return project.provider { project.findProperty(propName) as String? }.map { s ->
+        val p = s.takeIf { it.isNotBlank() } ?: run {
+            System.getenv(envVar).takeIf { it.isNotBlank() }
+        }
+        p?.let { Result.success(it) } ?: err
+    }.orElse(err)
+}
+
+internal fun Project.getPropOrEnv(propName: String, envVar: String? = null): String? =
+    propOrEnvProvider(propName, envVar).get().getOrNull()
+
+internal fun Project.getMandatoryPropOrEnv(propName: String, envVar: String? = null): String =
+    propOrEnvProvider(propName, envVar).get().getOrThrow()
